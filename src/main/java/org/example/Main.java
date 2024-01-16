fbd12b55c65265db0f8848202ac89dd0acb4c777
Chris_nkuutu <nkuutugeorge@gmail.com>
2024-01-16 22:47:56 +0100

    Update Action

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world my name is Jekyll mathafuckaz!");
+        System.out.println("Hello world my name is Jekyll!");
+        System.out.println("I am new in these ends");
+        System.out.println("But ready to learn and have fun");
+        System.out.println("and be super at it like they say be good");
+
     }
 }
