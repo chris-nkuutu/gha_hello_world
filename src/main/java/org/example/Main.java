265e6ae7543df5a9b454413a9e11d715dbdb4ef0
Chris_nkuutu <nkuutugeorge@gmail.com>
2024-01-16 22:25:11 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world my name is Jekyll mathafuckaz!");
+        System.out.println("Hello world my name is Jekyll m
     }
 }
