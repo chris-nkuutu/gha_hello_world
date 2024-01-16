cdc24d19ba8d6664fdd8f8582621df29a355ee95
Chris_nkuutu <nkuutugeorge@gmail.com>
2024-01-16 22:09:05 +0100

    Create a java-ci.yml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world my name is Jekyll!");
+        System.out.println("Hello world my name is Jekyll mathafuckaz!");
     }
 }
